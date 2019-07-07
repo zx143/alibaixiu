8b01ed728edef303da7de4a56d07ac65ad2cac91
zx143 <zgq15173465209@163.com>
2019-07-07 22:19:29 +0800

    仪表盘数据展示完成

---
+++ public/assets/js/index.js
@@ -0,0 +1,33 @@
+//获取文章数量
+$.ajax({
+    type: 'get',
+    url: '/posts/count',
+    success: function (response) {
+        //console.log(response);
+        //总数量
+        $("#sum").html(response.postCount);
+        //未审核文章数量
+        $("#audit").html(response.draftCount);
+    }
+});
+
+//获取分类数量
+$.ajax({
+    type: 'get',
+    url: '/categories/count',
+    success: function (response) {
+        //console.log(response);
+        $('#classify').html(response.categoryCount);
+    }
+});
+
+
+//获取评论数量
+$.ajax({
+    type: 'get',
+    url: '/comments/count',
+    success: function (response) {
+        console.log(response);
+        $('#sumComment').html(response.commentCount)
+    }
+})
